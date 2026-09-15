eb2ade17b22fcc4977ff3855c0192db9c2df9aa8
ZainabFasih <Z.Fasih.30533@khi.iba.edu.pk>
2026-09-15 19:26:40 +0500

    implemented find -exec and added time.txt

--- user/find.c
+++ user/find.c
@@ -3,13 +3,13 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
+#include "kernel/param.h"
 
 char *
 fmtname(char *path)
 {
   char *p;
 
-  // Find first character after last slash.
   for (p = path + strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
@@ -17,7 +17,29 @@ fmtname(char *path)
 }
 
 void
-find(char *path, char *target)
+run_exec(char **exec_argv, int exec_argc, char *matched_path)
+{
+  int pid;
+
+  if ((pid = fork()) == 0) {
+    char *argv2[MAXARG];
+    int i;
+    for (i = 0; i < exec_argc; i++) {
+      argv2[i] = exec_argv[i];
+    }
+    argv2[i++] = matched_path;
+    argv2[i] = 0;
+
+    exec(argv2[0], argv2);
+    fprintf(2, "find: exec %s failed\n", argv2[0]);
+    exit(1);
+  } else {
+    wait(0);
+  }
+}
+
+void
+find(char *path, char *target, char **exec_argv, int exec_argc)
 {
   char buf[512], *p;
   int fd;
@@ -38,7 +60,11 @@ find(char *path, char *target)
   switch (st.type) {
   case T_FILE:
     if (strcmp(fmtname(path), target) == 0) {
-      printf("%s\n", path);
+      if (exec_argc > 0) {
+        run_exec(exec_argv, exec_argc, path);
+      } else {
+        printf("%s\n", path);
+      }
     }
     break;
 
@@ -65,11 +91,15 @@ find(char *path, char *target)
       }
 
       if (strcmp(de.name, target) == 0) {
-        printf("%s\n", buf);
+        if (exec_argc > 0) {
+          run_exec(exec_argv, exec_argc, buf);
+        } else {
+          printf("%s\n", buf);
+        }
       }
 
       if (st.type == T_DIR) {
-        find(buf, target);
+        find(buf, target, exec_argv, exec_argc);
       }
     }
     break;
@@ -80,12 +110,27 @@ find(char *path, char *target)
 int
 main(int argc, char *argv[])
 {
-  if (argc != 3) {
-    fprintf(2, "Usage: find <path> <name>\n");
+  char **exec_argv = 0;
+  int exec_argc = 0;
+
+  if (argc < 3) {
+    fprintf(2, "Usage: find <path> <name> [-exec cmd ...]\n");
     exit(1);
   }
 
-  find(argv[1], argv[2]);
+  // check for -exec starting at argv[3]
+  if (argc > 3) {
+    if (strcmp(argv[3], "-exec") == 0) {
+      exec_argv = &argv[4];
+      exec_argc = argc - 4;
+      if (exec_argc <= 0) {
+        fprintf(2, "Usage: find <path> <name> -exec cmd ...\n");
+        exit(1);
+      }
+    }
+  }
+
+  find(argv[1], argv[2], exec_argv, exec_argc);
 
   exit(0);
 }
