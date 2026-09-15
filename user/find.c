4af878d41f507bd0c4afb9e65772c4f5a4c9878b
ZainabFasih <Z.Fasih.30533@khi.iba.edu.pk>
2026-09-15 20:14:24 +0500

    optional: uptime and regex-based find

--- user/find.c
+++ user/find.c
@@ -4,7 +4,60 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 #include "kernel/param.h"
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
 
+int
+match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
+}
+int
+anchored_match(char *re, char *text)
+{
+  char buf[128];
+
+  if (re[0] == '^' || strchr(re, '$'))
+    return match(re, text);
+
+  buf[0] = '^';
+  strcpy(buf + 1, re);
+  int len = strlen(buf);
+  buf[len] = '$';
+  buf[len + 1] = '\0';
+
+  return match(buf, text);
+}
 char *
 fmtname(char *path)
 {
@@ -59,7 +112,7 @@ find(char *path, char *target, char **exec_argv, int exec_argc)
 
   switch (st.type) {
   case T_FILE:
-    if (strcmp(fmtname(path), target) == 0) {
+if (anchored_match(target, fmtname(path))) {
       if (exec_argc > 0) {
         run_exec(exec_argv, exec_argc, path);
       } else {
@@ -90,7 +143,7 @@ find(char *path, char *target, char **exec_argv, int exec_argc)
         continue;
       }
 
-      if (strcmp(de.name, target) == 0) {
+if (anchored_match(target, de.name)) {
         if (exec_argc > 0) {
           run_exec(exec_argv, exec_argc, buf);
         } else {
